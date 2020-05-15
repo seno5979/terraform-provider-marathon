19d976190c5dded71ea44cca58243923e0e18855
Seno Setiadi <seno5979@gmail.com>
2020-05-15 15:28:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/nicgrayson/terraform-provider-marathon/marathon"
+	"github.com/seno5979/terraform-provider-marathon/marathon"
 )
 
 func main() {
